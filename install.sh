03b03caf59389af9f31ee246cd10821b6b887b33
Daniel <madstacks@users.noreply.github.com>
2014-02-26 19:40:22 -0800

    Update install file

--- install.sh
+++ install.sh
@@ -4,13 +4,13 @@
 sudo apt-get install -y keepassx git git-gui terminator python-pip autojump screen gnome-tweak-tool
 sudo apt-get install -y keychain nemo unity-tweak-tool build-essential mongodb realpath jq curl
 sudo apt-get install -y python-dev python3-dev slapd ldap-utils libldap2-dev libsasl2-modules-ldap libsasl2-dev
-sudo apt-get install -y openssh-server vim
+sudo apt-get install -y openssh-server vim traceroute tmux
 
 sudo pip install virtualenvwrapper
 
 # Set 3rd party repos
 sudo add-apt-repository -y ppa:webupd8team/java
-sudo add-apt-repository ppa:webupd8team/sublime-text-2
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add - 
@@ -18,8 +18,8 @@ sudo sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian saucy con
 sudo apt-get update
 
 # Install extra packages
-sudo apt-get install -y oracle-java7-installer oracle-java7-set-default sublime-text google-chrome-stable
-sudo apt-get install -y virtualbox-4.3 dkms
+sudo apt-get install -y oracle-java7-installer oracle-java7-set-default google-chrome-stable
+sudo apt-get install -y virtualbox-4.3 dkms sublime-text-installer
 
 ./symlinks.py
 
