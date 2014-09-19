a2fba19f990ac02f884c5f6ecc3bce2273cfb585
Daniel <madstacks@users.noreply.github.com>
2014-09-19 11:42:11 -0700

    Add ssh wrapper

--- bash/20-functions.bash
+++ bash/20-functions.bash
@@ -1,6 +1,5 @@
 # ssh known_hosts deletion helper
-sdk()
-{
+function sdk() {
     if echo $1 | grep -qE '^[0-9]+$|^[0-9]+,[0-9]+$' ; then
         sed -i "${1}d" ${HOME}/.ssh/known_hosts
     fi
@@ -8,8 +7,7 @@ sdk()
 
 
 # deposit rsa key to root@remotehost
-putkey()
-{
+function putkey() {
     host=$(echo $@ | cut -d@ -f2) # helpful for putkey !! or !$ when previous "ssh root@<host>" fails
     scp -o strictHostKeyChecking=no ${HOME}/.ssh/id_rsa.pub ${host}:my_rsa.pub >/dev/null 2>&1
     ssh -t -t -o strictHostKeyChecking=no $host \
@@ -18,15 +16,31 @@ putkey()
 
 
 # delete a local and remote git branch
-rmbranch()
-{
+function rmbranch() {
     git branch -D $1 && git push origin :$1
 }
 
 
 # Tag and push upstream
-gtag()
-{
+function gtag() {
     git tag -a $1 -m 'v$1'
     git push --tags upstream
+}
+
+# Reset tmux window name after exiting remote host
+# Add something like this to your ~/.ssh/config to set window name to the remote host's hostname
+# after you SSH in
+#
+# Host * !*github* !*bitbucket*
+#     PermitLocalCommand yes
+#     LocalCommand if [[ "$TERM" == screen* && "%r" != "git" ]]; then printf "\033k%h\033\\"; fi
+function ssh() {
+    local current_name=$(tmux display-message -p '#W')
+    local ssh_bin=$(which ssh)
+    $ssh_bin "$@"
+    [[ -n "$TMUX" ]] && tmux rename-window $current_name
+}
+
+function s() {
+    ssh "$@"
 }
