fab5eff0996e54fdd761be5b32a1cdf36df565ae
madstacks <madstacks@users.noreply.github.com>
2018-06-22 11:02:50 -0700

    Remove sublime configs and update tmux

--- bash/20-functions.bash
+++ bash/20-functions.bash
@@ -33,25 +33,11 @@ function rmbranch() {
 
 
 # Tag and push upstream
+# gtag 1.2.3
+# gtag 1.2.3 upstream
 function gtag() {
     git tag -a $1 -m 'v$1'
-    git push --tags upstream
-}
-
-# Reset tmux window name after exiting remote host
-# Add something like this to your ~/.ssh/config to set window name to the remote host's hostname
-# after you SSH in
-#
-# Host * !*github* !*bitbucket*
-#     PermitLocalCommand yes
-#     LocalCommand if [[ "$TERM" == screen* && "%r" != "git" ]]; then printf "\033k%h\033\\"; fi
-# function ssh() {
-#     local current_name=$(tmux display-message -p '#W')
-#     local ssh_bin=$(which ssh)
-#     $ssh_bin "$@"
-#     [[ -n "$TMUX" ]] && tmux rename-window $current_name
-# }
-
-function s() {
-    ssh "$@"
+    if [[ -n $2 ]]; then
+      git push --tags $2
+    elif
 }
